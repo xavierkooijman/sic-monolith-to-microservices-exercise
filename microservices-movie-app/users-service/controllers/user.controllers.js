6473f2b991cf82aec9acafded87700a690ebd926
xavierkooijman <xavierkooijman@gmail.com>
2025-10-20 15:05:44 +0100

    add, update and delete a user

--- microservices-movie-app/users-service/controllers/user.controllers.js
+++ microservices-movie-app/users-service/controllers/user.controllers.js
@@ -1,19 +1,42 @@
-const { getAllUsers, getUserById } = require('../models/user.models');
+const userModel = require("../models/user.models");
 
 // Controlador para obter todos os utilizadores
 function getUsers(req, res) {
-    const users = getAllUsers();
-    res.json(users);
+  const users = userModel.getAllUsers();
+  res.json(users);
 }
 
 // Controlador para obter um utilizador por ID
 function getUser(req, res) {
-    const user = getUserById(req.params.id);
-    if (user) {
-        res.json(user);
-    } else {
-        res.status(404).json({ error: "User not found" });
-    }
+  const user = userModel.getUserById(req.params.id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ error: "User not found" });
+  }
 }
 
-module.exports = { getUsers, getUser };
+function createUser(req, res) {
+  const newUser = userModel.addUser(req.body);
+  res.status(201).json(newUser);
+}
+
+function updateUser(req, res) {
+  const updatedUser = userModel.updateUser(req.params.id, req.body);
+  if (updatedUser) {
+    res.json(updatedUser);
+  } else {
+    res.status(404).json({ error: "User not found" });
+  }
+}
+
+function deleteUser(req, res) {
+  const deletedUser = userModel.deleteUser(req.params.id);
+  if (deletedUser) {
+    res.json("User deleted successfully");
+  } else {
+    res.status(404).json({ error: "User not found" });
+  }
+}
+
+module.exports = { getUsers, getUser, createUser, updateUser, deleteUser };
--- microservices-movie-app/users-service/routes/user.routes.js
+++ microservices-movie-app/users-service/routes/user.routes.js
@@ -4,5 +4,8 @@ const userController = require("../controllers/user.controllers");
 
 router.get("/", userController.getUsers);
 router.get("/:id", userController.getUser);
+router.post("/", userController.createUser);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
